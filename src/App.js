111984e6e374963cdd43169ec6be00b1c0df3052
Vinnakota Saikrishna <vinnakotavnsaikrishna@gmail.com>
2023-07-04 23:42:54 -0400

    feat: custom build with placeholder

--- src/App.js
+++ src/App.js
@@ -2,15 +2,22 @@
 
 import React, { Component } from 'react';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import Editor from 'ckeditor5-custom-build/build/ckeditor';
 
 class App extends Component {
     render() {
         return (
+            // TODO: Check why toolbox does not appear
+            // NOTE: ckeditor5/sample/index.html works as expected
             <div className="App">
                 <h2>Using CKEditor 5 build in React</h2>
                 <CKEditor
-                    editor={ ClassicEditor }
+                    editor={ Editor }
+                    config={{
+                        placeholderProps: {
+                            types: ["First Name", "Date"],
+                        }
+                    }}
                     data="<p>Hello from CKEditor 5!</p>"
                     onReady={ editor => {
                         // You can store the "editor" and use when it is needed.
